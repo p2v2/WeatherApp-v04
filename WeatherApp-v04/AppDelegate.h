8310784df2da1ccab695b6dc318f54a4eb09acd1
Preeti Powar <ppowar@linkedin.com>
2018-06-22 10:57:19 -0700

    Initial Commit

---
+++ WeatherApp-v04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp-v04
+//
+//  Created by Preeti Powar on 6/22/18.
+//  Copyright © 2018 Preeti Powar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp-v04/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp-v04
+//
+//  Created by Preeti Powar on 6/22/18.
+//  Copyright © 2018 Preeti Powar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
